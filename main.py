eca4b73a7eb0fa2ca15deded5c6e3f81e1be172d
FrancoMoggia <fraanmoggia@gmail.com>
2024-03-17 16:27:12 -0300

    Agrego userss

--- main.py
+++ main.py
@@ -18,3 +18,27 @@ for x in range(5):
     f"INSERT INTO transactions SET({service_id, })"
 
     pass
+
+# ---------------------------------------
+import random
+from faker import Faker
+
+def random_to_users(iterations):
+    fake = Faker()
+
+    id = 0
+    genders = ['Male', 'Female']
+    email_domains = ['@gmail.com', '@hotmail.com.ar', '@hotmail.com', '@yahoo.com.ar', '@itba.edu.ar', 'yahoo.com']
+
+    for _ in range(iterations):
+        id += 1
+        username = fake.user_name()
+        password = fake.password()
+        email_domain = random.choice(email_domains)
+        email = f"{username}{email_domain}"
+        birth_date = fake.date_of_birth(minimum_age=18, maximum_age=74)
+        gender = random.choice(genders)
+
+        print(f"INSERT INTO User (ID, Username, Password, Mail, Birth Date, Gender) VALUES ('{id}', '{username}', '{password}', '{email}', '{birth_date}', '{gender}');")
+
+random_to_users(5) 

3e2d10fe130cdf6b6280e8acec51e9bb96925815
JoaquinTRamos <ramossjoaquinn@gmail.com>
2024-03-17 15:54:49 -0300

    setup

---
+++ main.py
@@ -0,0 +1,20 @@
+# CODIGO
+
+# INFORMACION SENSIBLE --> DB, HOST, USERNAME, PASSWORD
+
+# script para generar informacion
+
+# Funciones para ingresar datos
+
+# --- La persona ingresa cantidad de nuevos usuarios, cantidad de nuevas transacciones, ingresar datos de producto
+
+cache = []
+
+for x in range(5):
+
+    if len(cache) == 0:
+        cache = request.get("SELECT id FROM service master")
+
+    f"INSERT INTO transactions SET({service_id, })"
+
+    pass
